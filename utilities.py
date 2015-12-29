27b2e26a37408d9a6e2736b35a657075a0243faa
mwas <francismwangi152@gmail.com>
2015-12-29 07:19:06 +0300

    refactored some of the utilities used help in logging

--- utilities.py
+++ utilities.py
@@ -16,13 +16,24 @@ class NotFound(object):
 not_found = NotFound()
 
 
-class LogDjangoEnvironment(logging.Filter):
+class LogDjangoSetting(logging.Filter):
 
     def __init__(self):
         settings = os.environ.setdefault("DJANGO_SETTINGS_MODULE", "")
         self.environment = settings.split('.')[-1]
-        super(LogDjangoEnvironment, self).__init__()
+        super(LogDjangoSetting, self).__init__()
 
     def filter(self, record):
         record.environment = self.environment
+        return True
+
+
+class LogOsEnvironment(logging.Filter):
+
+    def __init__(self, env_variable):
+        self.env_variable = os.environ.get(env_variable, None)
+        super(LogOsEnvironment, self).__init__()
+
+    def filter(self, record):
+        record.os_variable = self.env_variable
         return True
