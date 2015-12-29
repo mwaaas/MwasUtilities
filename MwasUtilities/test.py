4e9050cde32c52654e22be32dd93983d5c6b6ac9
mwas <francismwangi152@gmail.com>
2015-12-29 07:48:49 +0300

    test.py and utilities to be in a python package

--- MwasUtilities/test.py
+++ MwasUtilities/test.py
@@ -2,9 +2,9 @@ __author__ = 'Mwaaas'
 __email__ = 'francismwangi152@gmail.com'
 __phone_number__ = '+254702729654'
 
-
 import unittest
-from utilities import not_found
+
+from MwasUtilities.utilities import not_found
 
 
 class TestNotFound(unittest.TestCase):
